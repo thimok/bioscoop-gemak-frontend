1083e343efaefa5ea236c16f1692d397d804362c
Thimo Koolen <tkoolen1@student.avans.nl>
2017-12-06 22:12:21 +0100

    Fixed menu collapse

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,22 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DataStorageService} from "../shared/data-storage.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor(private dataStorageService: DataStorageService) { }
-  
-  onFetchData() {
-  	this.dataStorageService.getMovies();
-  	this.dataStorageService.getTheaters();
-  	this.dataStorageService.getScreenings();
-  }
-
-  ngOnInit() {
-  }
-
+	
+	private collapsed: boolean = true;
+	
+	constructor(private dataStorageService: DataStorageService) {
+	}
+	
+	onFetchData() {
+		this.dataStorageService.getMovies();
+		this.dataStorageService.getTheaters();
+		this.dataStorageService.getScreenings();
+	}
+	
+	toggleCollapsed() {
+		this.collapsed = !this.collapsed;
+		console.log(this.collapsed);
+	}
+	
+	isCollapsed() {
+		return this.collapsed;
+	}
+	
+	ngOnInit() {
+	}
+	
 }
